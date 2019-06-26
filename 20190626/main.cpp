be4548c28a72aaad6acc37dacaa333cb77baefac
Aime1007 <1310959322@qq.com>
2019-06-26 18:34:12 +0800

    Initial Commit

---
+++ 20190626/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  20190626
+//
+//  Created by s20181106275 on 2019/6/26.
+//  Copyright © 2019 s20181106275. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
